8a37d99870b07c6d02b48d8ac3e54aed5847006a
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-12-10 17:43:18 +0800

    Update next-started

---
+++ next-started/src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+}
+export default function Dashboard(){
+    return <h1>Hello, Dashboard Page!</h1>
+}
