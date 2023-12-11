8caebae08c85127f944c2c7cf581447e0acb3dd1
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-12-11 23:29:35 +0800

    Update react18-debugger

--- react18-debugger/src/App.js
+++ react18-debugger/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import './App.css';
+import Sub from "./Sub";
 
 function App() {
+  debugger;
+
   return (
     <div className="App">
+      <Sub></Sub>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span className="App-span">span </span>
+        <p className="App-ppp">ppp</p>
       </header>
     </div>
   );
---
+++ react18-debugger/src/Sub.js
@@ -0,0 +1,5 @@
+const Sub = ()=>{
+    return <h1>sub</h1>
+}
+
+export default Sub
